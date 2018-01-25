0fbe1288d326a7cd38ec1ebc5cd13306f219e181
Ampa22 <ampa_margalef@hotmail.com>
2018-01-25 16:49:12 +0100

    primer commit

--- a12/lanzar.php
+++ a12/lanzar.php
@@ -5,14 +5,14 @@ include ("a12.php");
 $tirada1 = new jugadorDado();
 
 for($i=1; $i<=12; $i++){ /*Quiero hacer 12 tiradas de dados, por lo tanto, establecemos un bucle que represente dichas tiradas*/
- // echo "Tirada".$i."<br>";/*Apuntamos y mostramos las tiradas numeradas, segun los numeros establecidos en el bucle, 
+ // echo "Tirada".$i."<br>";/*Apuntamos y mostramos las tiradas numeradas, segun los numeros establecidos en el bucle,
 							//cumplan la condicion, en este caso de 1 a 12*/
   $tirada1->tiraDado()."<br>";/*Mostramos el numero obtenido en las tiradas (azar)*/
-  
+
  }
 
 $tirada1->imprimirTiradas();
 
 echo $tirada1->calcularMediaTiradas();
-
+echo"Hola";
 ?>
